ed4ccbbb578301101f41d499b53a1d3976b6b3bb
arsalan.ahmed <arsalanabc@gmail.com>
2020-01-03 21:33:24 -0500

    Update CalculateApp method call

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,8 @@ import main.calculator_app.CalculatorApp;
 public class Main {
 
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        CalculatorApp calculatorApp = new CalculatorApp(calculator);
-        int result = calculatorApp.input(args[0]).process().output();
+        CalculatorApp calculatorApp = new CalculatorApp(new Calculator());
+        int result = calculatorApp.process(args[0]);
         System.out.println(result);
     }
 }
