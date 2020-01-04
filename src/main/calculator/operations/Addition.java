145f741adb0effdc74bd8ba05068544fa726e06f
arsalan.ahmed <arsalanabc@gmail.com>
2020-01-04 01:13:38 -0500

    Add @Override annotation

--- src/main/calculator/operations/Addition.java
+++ src/main/calculator/operations/Addition.java
@@ -1,6 +1,7 @@
 package main.calculator.operations;
 
 public class Addition implements Operation {
+    @Override
     public int calc (int a, int b){
         return a+b;
     }
--- src/main/calculator/operations/Multiplication.java
+++ src/main/calculator/operations/Multiplication.java
@@ -1,6 +1,8 @@
 package main.calculator.operations;
 
 public class Multiplication implements Operation {
+
+    @Override
     public int calc (int a, int b){
         return a*b;
     }
